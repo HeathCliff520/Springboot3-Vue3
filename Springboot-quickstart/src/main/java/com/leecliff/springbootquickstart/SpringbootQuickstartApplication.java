f1df35bd2a52c26b775ce197222cbef4d8655052
Heath Cliff <784361867@qq.com>
2023-11-18 14:27:58 +0800

    springboot-quickstart

---
+++ Springboot-quickstart/src/main/java/com/leecliff/springbootquickstart/SpringbootQuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.leecliff.springbootquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootQuickstartApplication.class, args);
+    }
+
+}
