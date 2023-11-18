0a679b9c336887d24edbc03740c34aa2cf5e80de
Heath Cliff <784361867@qq.com>
2023-11-18 14:57:54 +0800

    理解controller层的作用

--- Springboot-quickstart/src/main/java/com/leecliff/springbootquickstart/controller/Hellocontroller.java
+++ Springboot-quickstart/src/main/java/com/leecliff/springbootquickstart/controller/Hellocontroller.java
@@ -3,9 +3,10 @@ package com.leecliff.springbootquickstart.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //把Hellocontroller类交给springboot做成一个控制类
+@RestController //@RestController：把Hellocontroller类交给springboot做成一个控制类
 public class Hellocontroller {
-    @RequestMapping("/hello") //把请求路径及参数个springboot并将请求内容给hello方法以便hello方法处理并给出返回结果
+    //@RequestMapping("/hello")：把请求路径及参数个springboot并将请求内容给hello方法以便hello方法处理并给出返回结果
+    @RequestMapping("/hello")
     public String hello(){
         return "hello is from tomcat sever";
     }
